b874337d508370a5a6ecbc6f019b30f576bfe611
Nicholas Gonsalves <gonzo.nick123@gmail.com>
2023-12-03 13:34:58 +0000

    day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     // Part 1
     fn in_bounds(i: isize, j: isize, grid: &Vec<Vec<char>>) -> bool {
@@ -7,7 +9,14 @@ fn main() {
             && j < grid[0].len().try_into().unwrap()
     }
 
-    fn check_symbol_adjacent(i: isize, j: isize, length: isize, engine: &Vec<Vec<char>>) -> bool {
+    fn check_symbol_adjacent(
+        i: isize,
+        j: isize,
+        length: isize,
+        engine: &Vec<Vec<char>>,
+        gears: &mut HashMap<(usize, usize), Vec<i32>>,
+        buffer: &Vec<char>,
+    ) -> bool {
         // i & j are the final position of a set of characters making up the overall number
         // therefore we must also know the length of the set, so we can search the entire space
         let dirs: Vec<(isize, isize)> = vec![
@@ -26,6 +35,14 @@ fn main() {
                     let i_off: usize = (i + i_dir).try_into().unwrap();
                     let j_off: usize = (j + j_dir - offset).try_into().unwrap();
                     if !engine[i_off][j_off].is_alphanumeric() && engine[i_off][j_off] != '.' {
+                        if engine[i_off][j_off] == '*' {
+                            // Store gear ratios for part 2
+                            let value = buffer.iter().collect::<String>().parse::<i32>().unwrap();
+                            gears
+                                .entry((i_off, j_off))
+                                .or_insert(Vec::new())
+                                .push(value);
+                        };
                         return true;
                     }
                 }
@@ -43,6 +60,7 @@ fn main() {
     // Scan through arr, checking each number for an adjacent symbol
     let mut total = 0;
     let mut buffer = Vec::<char>::new();
+    let mut gears = HashMap::<(usize, usize), Vec<i32>>::new(); // (Part 2)
 
     for i in 0..engine.len() {
         for j in 0..engine[0].len() {
@@ -55,6 +73,8 @@ fn main() {
                     (j - 1).try_into().unwrap(),
                     buffer.len().try_into().unwrap(),
                     &engine,
+                    &mut gears,
+                    &buffer,
                 ) {
                     // This number is valid, so empty the buffer, and add the value to the total
                     total += buffer
@@ -76,6 +96,8 @@ fn main() {
                 (engine[0].len() - 1).try_into().unwrap(),
                 buffer.len().try_into().unwrap(),
                 &engine,
+                &mut gears,
+                &buffer,
             ) {
                 // This number is valid, so empty the buffer, and add the value to the total
                 total += buffer
@@ -88,5 +110,15 @@ fn main() {
         buffer.drain(0..);
     }
 
+    // Part 1
     println!("{}", total);
+
+    // Part 2
+    let gear_ratio_total: i32 = gears
+        .values()
+        .filter(|ratios| ratios.len() == 2)
+        .map(|ratios| ratios.iter().product::<i32>())
+        .sum();
+
+    println!("{}", gear_ratio_total);
 }
