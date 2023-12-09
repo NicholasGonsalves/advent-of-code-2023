cb212eb855fdabfa1a48fdb7b70a19a25a821a49
Nicholas Gonsalves <gonzo.nick123@gmail.com>
2023-12-09 15:00:12 +0000

    day8 part 2 lcm fun

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use num::integer::lcm;
 
 fn main() {
     // Part 1
@@ -16,11 +17,12 @@ fn main() {
     fn walk_graph(
         lr_index: usize,
         node: &str,
-        steps: u32,
-        lrorder: Vec<char>,
-        graph: HashMap<&str, (&str, &str)>,
-    ) -> u32 {
-        if node == "ZZZ" {
+        target: &str,
+        steps: u64,
+        lrorder: &Vec<char>,
+        graph: &HashMap<&str, (&str, &str)>,
+    ) -> u64 {
+        if node.ends_with(target) {
             return steps;
         }
         let dir = &lrorder[lr_index];
@@ -33,7 +35,7 @@ fn main() {
             lr_index if lr_index + 1 < lrorder.len() => lr_index + 1,
             _ => 0,
         };
-        return walk_graph(next_lr_index, next_node, steps + 1, lrorder, graph);
+        return walk_graph(next_lr_index, next_node, target, steps + 1, lrorder, graph);
     }
 
     let (lrorder_str, graph_str) = include_str!("day8.txt").split_once("\n\n").unwrap();
@@ -41,6 +43,30 @@ fn main() {
     let graph = parse_graph(graph_str);
     let lrorder = lrorder_str.chars().collect::<Vec<char>>();
 
-    let steps_part_1 = walk_graph(0, "AAA", 0, lrorder, graph);
+    let steps_part_1 = walk_graph(0, "AAA", "ZZZ", 0, &lrorder, &graph);
     println!("{:?}", steps_part_1);
+
+    // Part 2
+    let start_nodes = graph
+        .keys()
+        .into_iter()
+        .map(|c| c.clone())
+        .filter(|node| node.chars().nth(2).unwrap() == 'A')
+        .collect::<Vec<&str>>();
+
+    // We can't brute force, so we must find the cycle length of each start node, and take the LCM!
+    // Re-use existing walk graph function, but exit on first node that ends with Z
+    let steps_lcm_part_2: u64 = start_nodes.iter().map(|start_node| {
+        walk_graph(
+            0,
+            &start_node,
+            "Z",
+            0,
+            &lrorder,
+            &graph,
+        )
+    })
+    .fold(1, |acc, x| lcm(acc, x));
+
+    println!("{:?}", steps_lcm_part_2);
 }
