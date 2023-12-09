3b797dfafbc72e23e49aa2eb891d2929d409e328
Nicholas Gonsalves <gonzo.nick123@gmail.com>
2023-12-09 16:06:30 +0000

    day9 part1

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,25 +1,30 @@
 fn main() {
-    
-    
-    fn parse_history(line: &str) -> Vec<u64> {
-        line.split_ascii_whitespace().map(|value| value.parse().unwrap()).collect::<Vec<u64>>()
+    fn parse_history(line: &str) -> Vec<i64> {
+        line.split_ascii_whitespace()
+            .map(|value| value.parse().unwrap())
+            .collect::<Vec<i64>>()
     }
 
-    fn predict(history: Vec<u64>) -> u64 {
-        if history.iter().sum() == 0 {
-            // Bottom level reached
-
+    fn predict(history: Vec<i64>) -> i64 {
+        // Bottom level reached
+        if history.iter().all(|value| value == &0) {
+            return 0;
         };
 
+        let diffs = history[1..]
+            .iter()
+            .enumerate()
+            .map(|(i, value)| value - history[i])
+            .collect::<Vec<i64>>();
 
+        return predict(diffs) + history.last().unwrap();
     }
 
-    let prediction_sum: u64 = include_str!("day9.txt")
+    let prediction_sum: i64 = include_str!("day9.txt")
         .lines()
         .map(parse_history)
         .map(predict)
         .sum();
 
     println!("{}", prediction_sum);
-
 }
