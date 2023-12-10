8d6d85db89e2d59b19efb5817ccee42d9b06c95e
Nicholas Gonsalves <gonzo.nick123@gmail.com>
2023-12-10 20:48:38 +0000

    day10 part 1

---
+++ day10/src/main.rs
@@ -0,0 +1,117 @@
+use std::ops::Div;
+use tailcall::tailcall;
+
+const DOWN: (isize, isize) = (1, 0);
+const LEFT: (isize, isize) = (0, -1);
+const UP: (isize, isize) = (-1, 0);
+const RIGHT: (isize, isize) = (0, 1);
+
+const DIRS: [(isize, isize); 4] = [DOWN, LEFT, UP, RIGHT];
+
+type Pos = (isize, isize);
+
+fn find_start(grid: &Vec<Vec<char>>) -> Pos {
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] == 'S' {
+                return (i.try_into().unwrap(), j.try_into().unwrap());
+            }
+        }
+    }
+    panic!("Start character 'S' not found in grid!");
+}
+
+fn in_bounds(i: isize, j: isize, grid: &Vec<Vec<char>>) -> bool {
+    0 <= i && i < grid.len().try_into().unwrap() && 0 <= j && j < grid[0].len().try_into().unwrap()
+}
+
+fn locate_starting_step(pos: (isize, isize), grid: &Vec<Vec<char>>) -> (isize, isize) {
+    // Find first of two possible starting routes
+    for dir in DIRS {
+        if in_bounds(pos.0 + dir.0, pos.1 + dir.1, grid) {
+            let look = grid[(pos.0 + dir.0) as usize][(pos.1 + dir.1) as usize];
+            let look_pos = (pos.0 + dir.0, pos.1 + dir.1);
+            return match dir {
+                LEFT => match look {
+                    '-' | 'L' | 'F' => look_pos,
+                    _ => continue,
+                },
+                RIGHT => match look {
+                    '-' | 'J' | '7' => look_pos,
+                    _ => continue,
+                },
+                UP => match look {
+                    '|' | '7' | 'F' => look_pos,
+                    _ => continue,
+                },
+                DOWN => match look {
+                    '|' | 'L' | 'J' => look_pos,
+                    _ => continue,
+                },
+                _ => panic!("{:?} is not a valid unit direction!", dir),
+            };
+        }
+    }
+    panic!("We've looked in every direction, but we're stuck!")
+}
+
+fn next_step(pos: Pos, prev_pos: Pos, dir1: Pos, dir2: Pos) -> Pos {
+    if (pos.0 + dir1.0, pos.1 + dir1.1) != prev_pos {
+        (pos.0 + dir1.0, pos.1 + dir1.1)
+    } else {
+        (pos.0 + dir2.0, pos.1 + dir2.1)
+    }
+}
+
+#[tailcall] // required so we don't hit a stack overflow, force reuse of last stackframe!
+fn step(pos: Pos, mut visited: Vec<Pos>, start: Pos, grid: &Vec<Vec<char>>) -> Vec<Pos> {
+    // Walk through grid, keeping track of the path, until we get back to the start!
+    if pos == start && visited.len() > 0 {
+        return visited;
+    }
+
+    let current_symbol = grid[pos.0 as usize][pos.1 as usize];
+    let prev_pos = match visited.last() {
+        Some(pos) => pos.clone(),
+        None => start.clone(),
+    };
+
+    visited.push(pos);
+
+    let next_pos: Pos = match current_symbol {
+        'S' => locate_starting_step(pos, grid),
+        '|' => next_step(pos, prev_pos, UP, DOWN),
+        '-' => next_step(pos, prev_pos, LEFT, RIGHT),
+        'L' => next_step(pos, prev_pos, UP, RIGHT),
+        'J' => next_step(pos, prev_pos, UP, LEFT),
+        '7' => next_step(pos, prev_pos, LEFT, DOWN),
+        'F' => next_step(pos, prev_pos, DOWN, RIGHT),
+        _ => panic!("We shouldn't be on a non-pipe symbol!"),
+    };
+
+    println!("next {:?}", next_pos);
+
+    return step(next_pos, visited, start, grid);
+}
+
+fn main() {
+    // Part 1
+    // Load into grid
+    let grid = include_str!("day10.txt")
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    // Find start position
+    let start = find_start(&grid);
+
+    // Walk route!
+    // (we don't strictly need to remember entire route, 
+    //  we could reduce mem use by just remembering the previous point, and the number of steps)
+    let route = step(start, vec![], start, &grid);
+
+    // Find distance to furthest point
+    let furthest_dist = route.len().div(2);
+
+    println!("{:?}", furthest_dist);
+}
